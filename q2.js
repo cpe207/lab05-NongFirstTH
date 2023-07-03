fe5651c3ae33ae895e768640deae706ba2e656ac
NongFirstTH <TanabodeeChannel@gmail.com>
2023-07-03 15:43:22 +0700

    V1

--- q2.js
+++ q2.js
@@ -1,5 +1,6 @@
 function findTopNames(students) {
-  /* Your code here */
+  a = students.filter((x) => x.score > 8).map((x) => x.name)
+  return a 
 }
 
 const students1 = [
@@ -7,6 +8,7 @@ const students1 = [
   { name: "jane", score: 9 },
   { name: "jim", score: 8 },
 ];
+
 
 console.log(findTopNames(students1));
 
